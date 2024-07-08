aefb53d2a17fa9f35a01497291f54f445fc21096
Steev Vhoo <osteevvhoo@gmail.com>
2024-07-08 14:17:00 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+
+
+
+
+
+//Create express app
+const app = express()
+
+
+//Apply midlleware
+app.use(express.json());
+
+
+//Use Routes
+
+
+
+///Listening for incoming request
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+	console.log(`Running on Port ${PORT}`);
+});
